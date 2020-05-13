8c527f3467cc09cbaee5bab8022ce0213539618f
Subrat Behera <subrat.behera@s-force.org>
2020-05-13 12:40:06 +0530

    wrote the logic inside it

--- boot-feign-git/src/main/java/com/git/feign/test/controller/HealthController.java
+++ boot-feign-git/src/main/java/com/git/feign/test/controller/HealthController.java
@@ -1,5 +1,16 @@
 package com.git.feign.test.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
 public class HealthController {
+	
+	@GetMapping("/check")
+	public String checkHealth() {
+		return "UP";
+	}
 
 }
