44ce26c440ab7536ebca16d2a03e5e1ac3cb59f1
Subrat Behera <subrat.behera@s-force.org>
2020-05-13 12:07:33 +0530

    commited the code first time

---
+++ boot-feign-git/src/main/java/com/git/feign/test/BootFeignGitApplication.java
@@ -0,0 +1,13 @@
+package com.git.feign.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootFeignGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootFeignGitApplication.class, args);
+	}
+
+}
